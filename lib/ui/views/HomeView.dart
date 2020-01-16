1d1310b924f70c4a7d675b7557108ae7f3745523
yzylmz <yavuzyilmaz4190@gmail.com>
2020-01-16 22:34:13 +0300

    infinite scroll

--- lib/ui/views/HomeView.dart
+++ lib/ui/views/HomeView.dart
@@ -8,12 +8,49 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   final databaseReference = Firestore.instance;
 
-  int _currentIndex = 0;
-  Stream<QuerySnapshot> _jobStream =
-      Firestore.instance.collection('job').orderBy('createdDate', descending: true).snapshots();
+  static int scrollCount = 5;
+
+  ScrollController _scrollController = ScrollController();
+
+  Stream<QuerySnapshot> _jobStream = Firestore.instance
+      .collection('job')
+      .orderBy('createdDate', descending: true)
+      .limit(scrollCount)
+      .snapshots();
+
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _loadMore();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  _loadMore() {
+    int countDoc = Firestore.instance.collection('job').toString().length;
+
+    if (scrollCount < countDoc) {
+      setState(() {
+        scrollCount += 1;
+        _jobStream = Firestore.instance
+            .collection('job')
+            .orderBy('createdDate', descending: true)
+            .limit(scrollCount)
+            .snapshots();
+      });
+    }
+  }
 
   void gotoLoginPage() {
-    Navigator.of(context).pop(true);  
+    Navigator.of(context).pop(true);
   }
 
   Future<bool> _onWillPop() {
@@ -35,66 +72,67 @@ class _HomeViewState extends State<HomeView> {
           ),
         ) ??
         false;
-  } 
+  }
 
   @override
   Widget build(BuildContext context) {
     return new WillPopScope(
         onWillPop: _onWillPop,
         child: new Scaffold(
-            appBar: AppBar(
-              automaticallyImplyLeading: false,
-              backgroundColor: Colors.purple,
-              title: Text(
-                "İş İlanları",
-                style: TextStyle(color: Colors.black),
-              ),
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.amber[500],
+            title: Text(
+              "İş İlanları",
+              style: TextStyle(color: Colors.black),
             ),
-            body: Center(
-              child: StreamBuilder(
-                  stream: _jobStream,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (!snapshot.hasData) return new Text('Loading...');
-                    return new ListView(
-                      children: snapshot.data.documents.map((document) {
-                        return Center(
-                          child: Card(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                new ListTile(
-                                  leading: CircleAvatar(
-                                    backgroundImage: NetworkImage(
-                                        "https://bussplus.blob.core.windows.net/bussplusmain/0a6b764a_default_profile_image.png"),
-                                  ),
-                                  title: Text(document['title']),
-                                  subtitle: Text(document['explanation']),
-                                ),
-                                ButtonTheme.bar(
-                                  // make buttons use the appropriate styles for cards
-                                  child: ButtonBar(
-                                    children: <Widget>[
-                                      FlatButton(
-                                        child: const Text('Beğen'),
-                                        onPressed: () {/* ... */},
-                                      ),
-                                      FlatButton(
-                                        child: const Text('Yorum Yap'),
-                                        onPressed: () {/* ... */},
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
+          ),
+          body: Center(
+            child: StreamBuilder(
+              stream: _jobStream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) return new Text('Loading...');
+                return new ListView(
+                  controller: _scrollController,
+                  children: snapshot.data.documents.map((document) {
+                    return Center(
+                      child: Card(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            new ListTile(
+                              leading: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                    "https://bussplus.blob.core.windows.net/bussplusmain/0a6b764a_default_profile_image.png"),
+                              ),
+                              title: Text(document['title']),
+                              subtitle: Text(document['explanation']),
                             ),
-                          ),
-                        );
-                      }).toList(),
+                            ButtonTheme.bar(
+                              // make buttons use the appropriate styles for cards
+                              child: ButtonBar(
+                                children: <Widget>[
+                                  FlatButton(
+                                    child: const Text('Beğen'),
+                                    onPressed: () {/* ... */},
+                                  ),
+                                  FlatButton(
+                                    child: const Text('Yorum Yap'),
+                                    onPressed: () {/* ... */},
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     );
-                  },
-                ),
-              ),
-            ));
+                  }).toList(),
+                );
+              },
+            ),
+          ),
+        ));
   }
 }
